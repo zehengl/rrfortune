17b0534a9b45e87dc81d7c464e2ef9628780451b
Zeheng Li <imzehengl@gmail.com>
2021-01-07 15:03:34 -0700

    use seleniumbase

--- tests/test_rrfortune.py
+++ tests/test_rrfortune.py
@@ -1,74 +1,32 @@
 import os
 
 import rollbar
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-renren = "http://renren.com"
+from seleniumbase import BaseCase
 
 
-def test_rrfortune():
-    """
-    test for rrfortune
-    """
+class RenRenTestClass(BaseCase):
+    def test_fortune_collection(self):
+        rollbar.init(os.environ.get("rollbar"))
 
-    rollbar.init(os.environ.get("rollbar"))
+        try:
+            email = os.environ.get("email")
+            password = os.environ.get("password")
 
-    email = os.environ.get("email")
-    password = os.environ.get("password")
-    assert email and password
+            self.open("http://renren.com")
 
-    wait_time = int(os.environ.get("wait", 10))
+            self.wait_for_element("email", by=By.ID)
+            self.wait_for_element("password", by=By.ID)
+            self.type("email", email, by=By.ID)
+            self.type("password", password, by=By.ID)
+            self.click("login", by=By.ID)
 
-    try:
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
+            self.wait_for_element("assembleBtn", by=By.ID)
+            self.click("assembleBtn", by=By.ID)
 
-        driver = webdriver.Chrome(options=options)
-
-        driver.get(renren)
-
-        input_email = WebDriverWait(driver, wait_time).until(
-            EC.presence_of_element_located((By.ID, "email"))
-        )
-        input_password = WebDriverWait(driver, wait_time).until(
-            EC.presence_of_element_located((By.ID, "password"))
-        )
-        btn_login = WebDriverWait(driver, wait_time).until(
-            EC.presence_of_element_located((By.ID, "login"))
-        )
-
-        input_email.clear()
-        input_password.clear()
-        input_email.send_keys(email)
-        input_password.send_keys(password)
-        driver.execute_script("arguments[0].click()", btn_login)
-
-        btn_fortune = WebDriverWait(driver, wait_time).until(
-            EC.presence_of_element_located((By.ID, "assembleBtn"))
-        )
-
-        driver.execute_script("arguments[0].click()", btn_fortune)
-
-        popup = WebDriverWait(driver, wait_time).until(
-            EC.visibility_of_element_located((By.ID, "forPopupBox"))
-        )
-
-        passed = popup.value_of_css_property("display") == "block"
-        if not passed:
-            rollbar.report_message("Test Failed")
-        assert passed
-
-    except WebDriverException:
-        rollbar.report_message("Test Errored")
-        assert False
-
-    else:
-        rollbar.report_message("Test Passed")
-        assert True
-
-    finally:
-        driver.quit()
+            # self.wait_for_element("forPopupBox", by=By.ID)
+            self.assert_element_visible("forPopupBox", by=By.ID)
+        except:
+            rollbar.report_message("Test Errored")
+        else:
+            rollbar.report_message("Test Passed")
